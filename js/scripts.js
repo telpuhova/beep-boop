e7b462adca17b0300c49cb3a8f89053a6295d85d
telpuhova <telpuhova@gmail.com>
2017-12-08 12:19:33 -0800

    add spec: returning 'I'm sorry, Dave. I'm afraid I can't do that.' instead of numbers that contain a 3

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,16 @@ var beepBoop = function(number) {
 		var digits = (i.toString()).split("");
 		flag1 = false;
 		flag0 = false;
+		flag3 = false;
 		for (var j=0; j <= digits.length; j++) {
 			if (digits[j] === "1") {
 				flag1 = true;
 			}
 			if (digits[j] === "0") {
 				flag0 = true;
+			}
+			if (digits[j] === "3") {
+				flag3 = true;
 			}
 		}
 		if (flag1) {
@@ -20,8 +24,7 @@ var beepBoop = function(number) {
 		else if (flag0) {
 			result.push("beep");
 		}
-
-		else if (i === 3) {
+		else if (flag3) {
 			result.push("I'm sorry, Dave. I'm afraid I can't do that.");
 		}
 		else {
