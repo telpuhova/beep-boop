bbf66805ecb0f9eaa581d9dd5c4bf5122aa9d51a
telpuhova <telpuhova@gmail.com>
2017-12-08 11:39:39 -0800

    add spec: counting up to the provided number

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+var beepBoop = function(number) {
+	var result = [];
+	for (var i=0; i <= number; i++) {
+		result.push(i);
+	}
+	return result;
+}
+
+
 $("document").ready(function() {
 	$("#formBeep").submit(function(event) {
 		event.preventDefault();
